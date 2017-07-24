96635e4b2cd29b878ac2547d77b4a0cadd36ace0
Taylor Newill <tanewill@microsoft.com>
2017-07-24 08:26:22 -0400

    Update install-ccm.sh
    updated impi

--- RawStarCCMCluster/install-ccm.sh
+++ RawStarCCMCluster/install-ccm.sh
@@ -15,18 +15,22 @@ wget -q http://azbenchmarkstorage.blob.core.windows.net/cdadapcobenchmarkstorage
 
 tar -xf /mnt/resource/scratch/$DOWN -C /mnt/resource/scratch
 tar -xzf /mnt/resource/scratch/STAR-CCM+11.04.012_01_linux-x86_64-r8.tar.gz -C /mnt/resource/scratch/INSTALLERS/
+echo hello
+impi_version=`ls /opt/intel/impi`
+source /opt/intel/impi/${impi_version}/bin64/mpivars.sh
 
 cd /mnt/resource/scratch/INSTALLERS/starccm+_11.04.012/
-
+ls
 echo export PODKey=$LICIP >> /home/$USER/.bashrc
 echo export CDLMD_LICENSE_FILE=1999@flex.cd-adapco.com >> /home/$USER/.bashrc
 echo export HOSTS=/home/$USER/bin/nodenames.txt
-echo export INTELMPI_ROOT=/opt/intel/impi/5.1.3.181 >> /home/$USER/.bashrc
+echo export INTELMPI_ROOT=/opt/intel/impi/${impi_version} >> /home/$USER/.bashrc
 echo export I_MPI_FABRICS=shm:dapl >> /home/$USER/.bashrc
 echo export I_MPI_DAPL_PROVIDER=ofa-v2-ib0 >> /home/$USER/.bashrc
 echo export I_MPI_ROOT=/opt/intel/compilers_and_libraries_2016.2.181/linux/mpi >> /home/$USER/.bashrc
-echo export PATH=/mnt/resource/scratch/applications/STAR-CCM+11.04.012-R8/star/bin:/opt/intel/impi/5.1.3.181/bin64:$PATH >> /home/$USER/.bashrc
+echo export PATH=/mnt/resource/scratch/applications/STAR-CCM+11.04.012-R8/star/bin:/opt/intel/impi/${impi_version}/bin64:$PATH >> /home/$USER/.bashrc
 echo export I_MPI_DYNAMIC_CONNECTION=0 >> /home/$USER/.bashrc
+echo source /opt/intel/impi/${impi_version}/bin64/mpivars.sh
 echo '/mnt/resource/scratch/applications/STAR-CCM+11.04.012-R8/star/bin/starccm+ -np 8 -machinefile '$HOSTS' -power -podkey '$PODKey' -rsh ssh -mpi intel -cpubind bandwidth,v -mppflags " -ppn 8 -genv I_MPI_DAPL_PROVIDER=ofa-v2-ib0 -genv I_MPI_DAPL_UD=0 -genv I_MPI_DYNAMIC_CONNECTION=0" -batch runAndRecord.java /mnt/resource/scratch/benchmark/*.sim' >> /mnt/resource/scratch/benchmark/runccm_example.sh
 
 sh /mnt/resource/scratch/INSTALLERS/starccm+_11.04.012/STAR-CCM+11.04.012_01_linux-x86_64-2.5_gnu4.8-r8.bin -i silent -DINSTALLDIR=/mnt/resource/scratch/applications -DNODOC=true -DINSTALLFLEX=false
